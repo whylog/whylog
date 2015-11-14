1c281df7d72ff5e39654557e92cf6fc8a67f8bb7
Andrzej Górski <andrzejgorski@supermond.com>
2015-11-14 16:29:20 +0100

    Add setup.py
    Add tests
    
    Add module tests

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os.path
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+    setup(
+        name="whylog",
+        version="0.1",
+        author="ZPP team",
+        author_email="",
+        description="whylog v0.1",
+        # Space to fill by Ewa
+        license="",
+        url="https://github.com/9livesdata/whylog",
+        long_description=read('README'),
+        classifiers=[
+            "Development Status :: 0 - Alpha",
+        ],
+    )
