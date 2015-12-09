d1c65e7a72593075a910231ea619900c261a0a49
Grzegorz Konefał <gk346991@students.mimuw.edu.pl>
2015-12-09 21:32:49 +0100

    reverse_from_offset moved to BacktrackSearcher as private method

--- whylog/client/__init__.py
+++ whylog/client/__init__.py
@@ -21,34 +21,6 @@ class WhylogClient(AbstractClient):
 	def __init__(self, rulesbase, open_path):
 		self.rulesbase = rulesbase
 		self.open_path = open_path
-
-	def reverse_from_offset(self, offset, buf_size=8192):
-		"""
-		a generator that returns the lines of a file in reverse order
-		beginning with the specified offset
-		"""
-		with open(self.open_path) as fh:
-			fh.seek(offset)
-			total_size = remaining_size = fh.tell()
-			reverse_offset = 0
-			truncated = None
-			while remaining_size > 0:
-				reverse_offset = min(total_size, reverse_offset + buf_size)
-				fh.seek(total_size-reverse_offset, 0)
-				buffer = fh.read(min(remaining_size, buf_size))
-				lines = buffer.split('\n')
-				remaining_size -= buf_size
-				if truncated is not None:
-					if buffer[-1] is not '\n':
-						lines[-1] += truncated
-					else:
-						yield truncated
-				truncated = lines[0]
-				for index in xrange(len(lines) - 1, 0, -1):
-					if len(lines[index]):
-						yield lines[index]
-			yield truncated
 		
 	def get_cause(self, offset, vim_line):
-		for line in self.reverse_from_offset(offset):
-			return line
+		pass
