458fe232190543f0e7b9223dd413f6bb10e8e327
Grzegorz Konefał <konefalg@gmail.com>
2016-03-02 18:34:01 +0100

    Yapf changes

--- whylog/tests/tests_client/constants.py
+++ whylog/tests/tests_client/constants.py
@@ -1,5 +1,6 @@
 class AFewLinesLogParams:
     SINGLE_LINE_LENGTH = 10
 
+
 class test_paths:
-	path_test_files = ['whylog', 'tests', 'tests_client', 'test_files']
+    path_test_files = ['whylog', 'tests', 'tests_client', 'test_files']
