b6da61a9d548a69f75d82bcdda8bdb1fb4023e85
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-03-13 01:22:10 +0100

    Refactor of private methods

--- whylog/config/parsers.py
+++ whylog/config/parsers.py
@@ -112,30 +112,26 @@ class ConcatedRegexParser(object):
         for name, indexes in self._forward_parsers_indexes.items():
             regex_index, regex_group_number = indexes[0], indexes[1]
             if forward_groups[regex_index] is not None:
-                self._extract_regex_params_from_match(forward_groups, clues, name,
-                                                      regex_group_number, regex_index)
+                clues[name] = self._extract_regex_params(forward_groups, regex_group_number, regex_index)
                 if backward_groups[self._backward_parsers_indexes[name][0]] is None:
-                    left, right = self._calculate_range_for_search(backward_groups, clues, name)
+                    left, right = self._calculate_range_for_brute_search(backward_groups, clues, name)
                     self._brute_subregexes_matching(clues, left, right, line)
                 break
         return clues
 
-    def _calculate_range_for_search(self, backward_groups, clues, name):
+    def _calculate_range_for_brute_search(self, backward_groups, clues, name):
         left = self._numbers_in_list[name] + 1
         for parser in reversed(self._parsers):
             if backward_groups[self._backward_parsers_indexes[parser.name][0]] is not None:
                 regex_index = self._backward_parsers_indexes[parser.name][0]
                 regex_group_number = self._backward_parsers_indexes[parser.name][1]
-                self._extract_regex_params_from_match(backward_groups, clues, parser.name,
-                                                      regex_group_number, regex_index)
+                clues[parser.name] = self._extract_regex_params(backward_groups, regex_group_number, regex_index)
                 right = self._parsers.index(parser) - 1
                 break
         return left, right
 
-    def _extract_regex_params_from_match(self, groups, clues, parser_name, regex_group_number,
-                                         regex_index):
-        clues[parser_name] = [groups[i]
-                              for i in range(regex_index + 1, regex_index + regex_group_number + 1)]
+    def _extract_regex_params(self, groups, regex_group_number, regex_index):
+        return [groups[i] for i in range(regex_index + 1, regex_index + regex_group_number + 1)]
 
     def _brute_subregexes_matching(self, clues, left, right, line):
         for i in range(left, right + 1):
