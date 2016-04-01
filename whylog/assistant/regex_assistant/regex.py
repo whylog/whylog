7e475c16434d29307c8d8ed760e5e5494d904d60
epawlowska <epawlowska@users.noreply.github.com>
2016-04-01 21:24:36 +0200

    RegexAssistent moved to own directory

--- whylog/assistant/regex_assistant/regex.py
+++ whylog/assistant/regex_assistant/regex.py
@@ -1,8 +1,10 @@
-class NoDateGroupException(Exception):
+class NoDateGroupError(Exception):
     def __init__(self, line_content, regex, line_index):
         self.line_content = line_content
         self.regex = regex
         self.line_index = line_index
 
     def __str__(self):
-        return 'No date group in regex. Regex: %. Line content: %' % (self.regex, self.line_content)
+        return 'No date group in regex. Regex: %s. Line content: %s' % (
+            self.regex, self.line_content
+        )
