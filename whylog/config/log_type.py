1aa5404edf59f5a5ba89e5459472eb470af38bb8
krzysztof <krzysztof@krzysztof.krzysztof>
2016-04-04 10:03:03 +0200

    Mocked next file to parse

--- whylog/config/log_type.py
+++ whylog/config/log_type.py
@@ -2,3 +2,7 @@ class LogType(object):
     def __init__(self, name, filename_matchers):
         self._name = name
         self._filename_matchers = filename_matchers
+
+    #It will be generator
+    def get_next_file_to_parse(self):
+        return 'localhost', 'node_1.log'
