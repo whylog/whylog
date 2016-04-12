99fb79a6eaa052aab255cde0783a31a4dda4c08b
epawlowska <epawlowska@users.noreply.github.com>
2016-04-12 17:24:35 +0200

    Changes in teacher interface

--- whylog/teacher/__init__.py
+++ whylog/teacher/__init__.py
@@ -7,20 +7,16 @@ class Teacher(object):
     Enable teaching new rule. One Teacher per one entering rule.
     """
 
-    def __init__(self, id_to_line_dict, effect_id, config, pattern_assistant):
-        self._lines = id_to_line_dict.copy()
-        self.rule_intent = UserRuleIntent(effect_id)
+    def __init__(self, config, pattern_assistant):
         self.config = config
         self.pattern_assistant = pattern_assistant
+        self._lines = {}  # line_id to line_object dict
 
-    def add_line(self, line_id, line_object):
-        self._lines[line_id] = line_object
+    def add_line(self, line_id, line_object, effect=False):
+        pass
 
     def remove_line(self, line_id):
-        del self._lines[line_id]
-        if line_id == self.effect_id:
-            # TODO: do something that represents warning "No effect line, remember to add it!"
-            pass
+        pass
 
     def update_pattern(self, line_id, proposed_pattern):
         """
@@ -34,13 +30,22 @@ class Teacher(object):
         """
         pass
 
+    def remove_group(self, line_id, group_id):
+        """
+        Improves text patterns by removing groups corresponding to params in text.
+        """
+        pass
+
     def guess_pattern(self, line_id):
         """
         Guess text pattern for line text.
         """
         pass
 
-    def set_convertion(self, group, conversion):
+    def set_pattern_name(self, line_id, name):
+        pass
+
+    def set_converter(self, line_id, group, converter):
         pass
 
     def set_primary_key(self, groups):
@@ -67,6 +72,16 @@ class Teacher(object):
         Verifies if text patterns and constraints meet all requirements.
         E.g it is required text pattern match its line in one way only.
         """
+        pass
+
+    def get_rule(self):
+        """
+        Creates rule for Front, that will be shown to user
+        """
+        return create_sample_rule()
+        pass
+
+    def test_rule(self):
         pass
 
     def save(self):
