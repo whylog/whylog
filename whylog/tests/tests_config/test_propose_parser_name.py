e370f42918c555e314b2313972057f5597644620
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-04-21 01:46:20 +0200

    Test refactor

--- whylog/tests/tests_config/test_propose_parser_name.py
+++ whylog/tests/tests_config/test_propose_parser_name.py
@@ -1,7 +1,6 @@
 import os.path
 from unittest import TestCase
 
-
 from whylog.config import YamlConfig
 
 path_test_files = ['whylog', 'tests', 'tests_config', 'test_files']
@@ -25,19 +24,16 @@ class TestBasic(TestCase):
         assert not self.config.is_free_parser_name('connectionerror')
         assert not self.config.is_free_parser_name('datamigration')
 
+    def create_proposed_name_request(self, black_list):
+        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
+        black_list.add(proposed_name)
+        return proposed_name
+
     def test_simple_proposed_name(self):
         black_list = set()
-        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
-        assert proposed_name == 'connection_error'
-
-        black_list.add(proposed_name)
-        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
-        assert proposed_name == 'error_occurred'
-
-        black_list.add(proposed_name)
-        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
-        assert proposed_name == 'occurred_on'
-
-        black_list = black_list.union(set(['occurred_on', 'on_host', 'host_name']))
-        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
-        assert proposed_name == 'connection_error1'
+        assert self.create_proposed_name_request(black_list) == 'connection_error'
+        assert self.create_proposed_name_request(black_list) == 'error_occurred'
+        assert self.create_proposed_name_request(black_list) == 'occurred_on'
+        black_list = black_list.union(set(['on_host', 'host_name']))
+        assert self.create_proposed_name_request(black_list) == 'connection_error1'
+        assert self.create_proposed_name_request(black_list) == 'connection_error2'
