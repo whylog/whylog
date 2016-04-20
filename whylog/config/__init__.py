aa0c8fef92ac5199e2266e46cc0bfa9106628332
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-04-21 01:40:25 +0200

    proposed name implementation

--- whylog/config/__init__.py
+++ whylog/config/__init__.py
@@ -3,7 +3,9 @@ from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 from datetime import datetime
 
+import re
 import six
+import uuid
 import yaml
 
 from whylog.assistant.exceptions import UnsupportedAssistantError
@@ -256,8 +258,40 @@ class AbstractConfig(object):
     def is_free_parser_name(self, parser_name):
         return parser_name not in self._parsers
 
-    def propose_parser_name(self, line, black_list):
-        pass
+    def _is_unique_parser_name(self, parser_name, black_list):
+        return self.is_free_parser_name(parser_name) and (parser_name not in black_list)
+
+    def propose_parser_name(self, line, regex_str, black_list):
+        building_words = AbstractConfig._get_building_words(line, regex_str)
+        if not building_words:
+            return uuid.uuid4()
+        if len(building_words) == 1:
+            if self._is_unique_parser_name(building_words[0], black_list):
+                return building_words[0]
+            else:
+                return self._find_free_by_number_appending(building_words[0], black_list)
+        for i in range(len(building_words) - 1):
+            propsed_name = building_words[i] + '_' + building_words[i + 1]
+            if self._is_unique_parser_name(propsed_name, black_list):
+                return propsed_name
+        return self._find_free_by_number_appending(building_words[0] + '_' + building_words[1], black_list)
+
+    @classmethod
+    def _get_building_words(cls, line, regex_str):
+        regex = re.compile(regex_str)
+        matched = regex.match(line)
+        if matched is not None:
+            groups = matched.groups()
+            for i in six.moves.range(len(groups)):
+                line = line.replace(groups[i], '')
+        line = re.sub('[,.;:]', '', line)
+        return [word.lower() for word in line.split()]
+
+    def _find_free_by_number_appending(self, word, black_list):
+        for i in six.moves.range(len(black_list) + 1):
+            propsed_name = word + str(i + 1)
+            if self._is_unique_parser_name(propsed_name, black_list):
+                return propsed_name
 
 
 @six.add_metaclass(ABCMeta)
--- whylog/tests/tests_config/test_propose_parser_name.py
+++ whylog/tests/tests_config/test_propose_parser_name.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import os.path
 from unittest import TestCase
 
@@ -17,6 +16,7 @@ class TestBasic(TestCase):
         log_type_path = os.path.join(path, 'log_types.yaml')
 
         cls.sample_line1 = "2016-04-12 23:54:45 Connection error occurred on comp1. Host name: host1"
+        cls.regex1 = "^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d) Connection error occurred on (.*)\. Host name: (.*)$"
 
         cls.config = YamlConfig(parsers_path, rules_path, log_type_path)
 
@@ -25,3 +25,19 @@ class TestBasic(TestCase):
         assert not self.config.is_free_parser_name('connectionerror')
         assert not self.config.is_free_parser_name('datamigration')
 
+    def test_simple_proposed_name(self):
+        black_list = set()
+        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
+        assert proposed_name == 'connection_error'
+
+        black_list.add(proposed_name)
+        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
+        assert proposed_name == 'error_occurred'
+
+        black_list.add(proposed_name)
+        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
+        assert proposed_name == 'occurred_on'
+
+        black_list = black_list.union(set(['occurred_on', 'on_host', 'host_name']))
+        proposed_name = self.config.propose_parser_name(self.sample_line1, self.regex1, black_list)
+        assert proposed_name == 'connection_error1'
