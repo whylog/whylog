b741eb857cde0dc2c37f47e69929ff968dee66f7
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-04-23 14:12:48 +0200

    Rename constant

--- whylog/config/parser_name_manager.py
+++ whylog/config/parser_name_manager.py
@@ -1,9 +1,9 @@
 import re
 
-WORDS_IN_NAME = 2
-
 
 class ParserNameManager(object):
+    WORDS_COUNT_IN_NAME = 2
+
     def __init__(self, parsers):
         self._parsers_name = set()
         for parser_name in parsers:
@@ -15,18 +15,18 @@ class ParserNameManager(object):
     def is_free_parser_name(self, parser_name, black_list):
         return (parser_name not in self._parsers_name) and (parser_name not in black_list)
 
-    def _create_name_from_words(self, words, words_in_name, black_list):
-        if len(words) <= words_in_name:
+    def _create_name_from_words(self, words, black_list):
+        if len(words) <= ParserNameManager.WORDS_COUNT_IN_NAME:
             proposed_name = '_'.join(words)
         else:
-            proposed_name = '_'.join(words[i] for i in range(words_in_name))
+            proposed_name = '_'.join(words[i] for i in range(ParserNameManager.WORDS_COUNT_IN_NAME))
         if self.is_free_parser_name(proposed_name, black_list):
             return proposed_name
         return self._find_free_by_number_appending(proposed_name, black_list)
 
     def propose_parser_name(self, line, regex_str, black_list):
         building_words = ParserNameManager._get_building_words(line, regex_str) or ['parser_name']
-        return self._create_name_from_words(building_words, WORDS_IN_NAME, black_list)
+        return self._create_name_from_words(building_words, black_list)
 
     @classmethod
     def _get_building_words(cls, line, pattern_str):
