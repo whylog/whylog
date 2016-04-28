2aed0d1e29dedb6790477b720e4f1127afce04fa
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2016-04-28 18:33:10 +0200

    Simple avoid to parse the same file many times

--- whylog/config/log_type.py
+++ whylog/config/log_type.py
@@ -4,5 +4,9 @@ class LogType(object):
         self.filename_matchers = filename_matchers
 
     def files_to_parse(self):
+        parsed_files = set()
         for matcher in self.filename_matchers:
-            return matcher.get_matched_files()
+            for file_source in matcher.get_matched_files():
+                if file_source not in parsed_files:
+                    parsed_files.add(file_source)
+                    yield file_source
