80fafa15f3af28dbbcce3502e2345548af1a9e73
Grzegorz Konefał <konefalg@gmail.com>
2016-04-28 14:13:12 +0200

    Another way of skipping tests

--- whylog/tests/tests_log_reader/__init__.py
+++ whylog/tests/tests_log_reader/__init__.py
@@ -2,7 +2,6 @@ import os.path
 from unittest import TestCase
 
 from generator import generate, generator
-from nose.plugins.skip import SkipTest
 
 from whylog.config import YamlConfig
 from whylog.front.utils import FrontInput
@@ -34,44 +33,41 @@ class TestBasic(TestCase):
         return [line.rstrip('\n') for line in open(file_path)][-1]
 
     @generate(
-        '001_most_basic',
-        '002_match_latest',
+        # '001_most_basic',
+        # '002_match_latest',
         '003_match_time_range',
-        '005_match_tree',
-        '006_match_parameter',
-        '007_match_or',
-        '008_match_and',
-        '009_match_negation',
-        '010_multiple_files',
-        '011_different_entry',
-        '012_multiple_rulebooks',
-        '013_match_and_incomplete',
+        # '005_match_tree',
+        # '006_match_parameter',
+        # '007_match_or',
+        # '008_match_and',
+        # '009_match_negation',
+        # '010_multiple_files',
+        # '011_different_entry',
+        # '012_multiple_rulebooks',
+        # '013_match_and_incomplete',
     )
     def test_one(self, test_name):
+        # paths files setup
         prefix_path = os.path.join(*TestPaths.path_test_files)
         path = os.path.join(prefix_path, test_name)
         input_path = os.path.join(path, 'input.txt')
         output_path = os.path.join(path, 'expected_output.txt')
         log_file = os.path.join(path, 'node_1.log')
 
-        # TODO this 'if' is temporary, remove this later
-        if not test_name == '003_match_time_range':
-            raise SkipTest("Functionality not implemented yet")
-
+        # gathering information about effect line
         line_number = self._get_cause_line_number(input_path)
         line_content = self._get_concrete_line_from_file(log_file, line_number)
         effect_line_offset = self._deduce_line_offset(log_file, line_number)
 
+        # preparing Whylog structures, normally prepared by Front
         whylog_config = YamlConfig(*ConfigPathFactory.get_path_to_config_files(path))
         log_reader = LogReader(whylog_config)
         line = FrontInput(effect_line_offset, line_content, prefix_path)
 
+        # action and checking the result
         result = log_reader.get_causes(line)
-
-        if test_name == '003_match_time_range':
-            # TODO this 'if' is temporary, remove this later
-            assert result
-            assert result[0].lines[0].line_content == self._get_last_line_from_file(output_path)
-            # ^ hack basing on that for test 003 last line of expected_output.txt is a cause line
-            # FIXME it is not appropriate for each test
-            # FIXME checking up correctness should be more advanced for more advanced tests
+        assert result
+        assert result[0].lines[0].line_content == self._get_last_line_from_file(output_path)
+        # ^ hack basing on that for test 003 last line of expected_output.txt is a cause line
+        # FIXME it is not appropriate for each test
+        # FIXME checking up correctness should be more advanced for more advanced tests
