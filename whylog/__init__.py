c8f6d7a23b53a75b9e1b30be7d0fdfb031faead4
Andrzej Górski <andrzejgorski@supermond.com>
2016-04-30 17:53:20 +0200

    simple fix in imports

--- whylog/__init__.py
+++ whylog/__init__.py
@@ -1,5 +1,5 @@
 from whylog.config import LineSource
-from whylog.config.filename_matchers import WildCardFilenameMatcherFactory
+from whylog.config.filename_matchers import WildCardFilenameMatcher
 from whylog.config.log_type import LogType
 from whylog.front.utils import FrontInput
 from whylog.front.whylog_factory import whylog_factory
@@ -12,4 +12,4 @@ assert LogReader
 assert Teacher
 assert whylog_factory
 assert LogType
-assert WildCardFilenameMatcherFactory
+assert WildCardFilenameMatcher
