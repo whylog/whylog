7e6364bd0d88aae224d6e539fbfe9766be320cec
epawlowska <epawlowska@users.noreply.github.com>
2016-05-07 12:47:04 +0200

    PatternMatch as class

--- whylog/assistant/pattern_match.py
+++ whylog/assistant/pattern_match.py
@@ -2,4 +2,13 @@ from collections import namedtuple
 
 ParamGroup = namedtuple('ParamGroup', ['content', 'converter'])
 
-PatternMatch = namedtuple('PatternMatch', ['line_text', 'pattern', 'param_groups'])
+
+class PatternMatch(object):
+    """
+    :type param_groups: dict[int, ParamGroup]
+    """
+
+    def __init__(self, line_text, pattern, param_groups):
+        self.line_text = line_text
+        self.pattern = pattern
+        self.param_groups = param_groups
