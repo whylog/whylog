cb5ca1bf3e001fad9cc20ebe36e5a1ed6dbbb7dd
epawlowska <epawlowska@users.noreply.github.com>
2016-05-15 16:39:58 +0200

    Rule update tests

--- whylog/tests/tests_teacher/__init__.py
+++ whylog/tests/tests_teacher/__init__.py
@@ -1,13 +1,17 @@
 import os
 from unittest import TestCase
 
+from whylog.assistant.const import ConverterType
 from whylog.assistant.pattern_match import ParamGroup
 from whylog.assistant.regex_assistant import RegexAssistant
+from whylog.assistant.regex_assistant.exceptions import NotMatchingRegexError
+from whylog.assistant.regex_assistant.regex import create_obvious_regex
 from whylog.config import YamlConfig
 from whylog.config.investigation_plan import LineSource
 from whylog.constraints import IdenticalConstraint
 from whylog.front.utils import FrontInput
 from whylog.teacher import Teacher
+from whylog.teacher.exceptions import NotUniqueParserName
 from whylog.teacher.user_intent import UserConstraintIntent, UserParserIntent
 from whylog.tests.utils import ConfigPathFactory
 
@@ -57,12 +61,12 @@ class TestBase(TestCase):
         cause2_line_source = LineSource('sample_host2', 'sample_path2')
         cause2_offset = 21
         cause2_front_input = FrontInput(cause2_offset, cause2_line_content, cause2_line_source)
-        cause2_id = 2
-        self.teacher.add_line(cause2_id, cause2_front_input)
+        self.cause2_id = 2
+        self.teacher.add_line(self.cause2_id, cause2_front_input)
         cause2_pattern = r'^([0-9]{4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}) Data migration to (.*) failed in test (.*)$'
-        self.teacher.update_pattern(cause2_id, cause2_pattern)
+        self.teacher.update_pattern(self.cause2_id, cause2_pattern)
 
-        self.identical_groups = [(self.cause1_id, 2), (cause2_id, 2)]
+        self.identical_groups = [(self.cause1_id, 2), (self.cause2_id, 2)]
 
     def tearDown(self):
         self._clean_test_files()
@@ -94,6 +98,57 @@ class TestParser(TestBase):
             self.effect_front_input.line_source,
         )  # yapf: disable
         assert wanted_effect_parser == effect_parser
+
+    def test_setting_parser_name(self):
+        effect_parser_name = self.teacher.get_rule().parsers[self.effect_id].pattern_name
+        new_name = effect_parser_name + '_hello'
+        self.teacher.set_pattern_name(self.effect_id, new_name)
+        rule = self.teacher.get_rule()
+        assert new_name == rule.parsers[self.effect_id].pattern_name
+
+        self.assertRaises(
+            NotUniqueParserName, self.teacher.set_pattern_name, self.cause1_id, new_name
+        )
+
+    def test_setting_converter(self):
+        parser = self.teacher.get_rule().parsers[self.cause2_id]
+        new_converter = ConverterType.TO_FLOAT
+        assert not new_converter == parser.groups[3].converter
+
+        self.teacher.set_converter(self.cause2_id, 3, new_converter)
+        parser = self.teacher.get_rule().parsers[self.cause2_id]
+        assert new_converter == parser.groups[3].converter
+
+    def test_setting_primary_key(self):
+        parser = self.teacher.get_rule().parsers[self.cause1_id]
+        new_primary_key_groups = [1, 2]
+        assert not new_primary_key_groups == parser.primary_key_groups
+        self.teacher.set_primary_key(self.cause1_id, new_primary_key_groups)
+        parser = self.teacher.get_rule().parsers[self.cause1_id]
+        assert new_primary_key_groups == parser.primary_key_groups
+
+    def test_setting_log_type(self):
+        new_log_type = '_very_low_likely_log_type'
+        self.teacher.set_log_type(self.effect_id, new_log_type)
+        parser = self.teacher.get_rule().parsers[self.effect_id]
+        assert new_log_type == parser.log_type_name
+
+    def test_update_pattern(self):
+        new_effect_pattern = r'^([0-9]{4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}) Error occurred in (.*) test$'
+        self.teacher.update_pattern(self.effect_id, new_effect_pattern)
+        updated_pattern = self.teacher.get_rule().parsers[self.effect_id].pattern
+        assert new_effect_pattern == updated_pattern
+
+        not_matching_pattern = new_effect_pattern + 'not_matching_part_of_regex'
+        self.assertRaises(
+            NotMatchingRegexError, self.teacher.update_pattern, self.effect_id, not_matching_pattern
+        )
+
+    def test_guess_patterns(self):
+        effect_guessed_patterns = self.teacher.guess_patterns(self.effect_id)
+        assert effect_guessed_patterns > 2
+        effect_obvious_regex = create_obvious_regex(self.effect_front_input.line_content)
+        assert effect_obvious_regex in effect_guessed_patterns
 
 
 class TestConstraints(TestBase):
