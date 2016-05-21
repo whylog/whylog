83ed86ed76b7485a143a9779101ff0a17b32decd
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-21 22:41:58 +0200

    Isort ????

--- whylog/constraints/constraint_manager.py
+++ whylog/constraints/constraint_manager.py
@@ -1,7 +1,12 @@
 import six
 
-from whylog.constraints import (DifferentConstraint, HeteroConstraint,  # yapf: disable
-                                IdenticalConstraint, TimeConstraint, ValueDeltaConstraint)  # yapf: disable
+from whylog.constraints import (
+    DifferentConstraint,
+    HeteroConstraint,
+    IdenticalConstraint,
+    TimeConstraint,
+    ValueDeltaConstraint
+)
 from whylog.constraints.const import ConstraintType
 from whylog.constraints.exceptions import UnsupportedConstraintTypeError
 
