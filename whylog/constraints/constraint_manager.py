e887856356af1c778ce71da3a2556dd688243650
Andrzej Górski <andrzejgorski@supermond.com>
2016-05-21 22:36:43 +0200

    disable yapf

--- whylog/constraints/constraint_manager.py
+++ whylog/constraints/constraint_manager.py
@@ -1,7 +1,7 @@
 import six
 
 from whylog.constraints import (DifferentConstraint, HeteroConstraint, IdenticalConstraint,
-                                TimeConstraint, ValueDeltaConstraint)
+                                TimeConstraint, ValueDeltaConstraint) # yapf: disable
 from whylog.constraints.const import ConstraintType
 from whylog.constraints.exceptions import UnsupportedConstraintTypeError
 
